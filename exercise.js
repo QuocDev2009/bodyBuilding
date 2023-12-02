9c1936b627fa920113560ffa590bd97c5c5f0741
QuocDev2009 <143098433+QuocDev2009@users.noreply.github.com>
2023-12-02 13:32:55 +0700

    Create button

--- exercise.js
+++ exercise.js
@@ -2,49 +2,57 @@ function exercises(arg){
     var form = document.querySelector(arg.form)
     var exerName = document.querySelector(arg.exerName)
     var exerTime = document.querySelector(arg.exerTime)
-    var audio = form.querySelector('audio')
     var exerImg = form.querySelector('img')
+    var controlsBtn = document.querySelector(arg.controlsBtn)
     var exerList = []
     const restForm = {
         name : arg.restHeading,
         time : arg.timeRest,
         img: arg.imgRest
     }
+    // append elements to exerList
     for (var exer of arg.exerList){
         for (var timesRange = 0; timesRange < (exer.times || 1); timesRange++){
             exerList.push(exer, restForm)
         }
     }
     exerList.pop()
+    // loop
+    var isFirstElement = true
     function loop(index){
         var exer =  exerList[index]
         if (exer){
-            var promise = Promise.resolve()
+            var timeRange = exer.time
+            exerTime.textContent = timeRange
             exerName.textContent = exer.name
-            exerTime.textContent = exer.time
             exerImg.src = exer.img
-            promise
-                .then(function(){
-                    return new Promise(function(resolve){
-                        var i = exer.time - 1
-                        var tmp = setInterval(function(){
-                            exerTime.textContent = i--
-                        }
-                        , 1000)
-                        setTimeout(resolve, exer.time*1000)
-                        setTimeout(function(){
-                            clearInterval(tmp)
-                        },exer.time*1000)
-                    })
-                })
-                .then(function(){
-                    // code
-                    audio.play()
-                    setTimeout(function(){
-                        audio.pause()
-                    }, 5000)
-                    loop(index+1)
-                })
+            // count down
+            function countDown(){
+                var tmp = setInterval(function(){
+                    exerTime.textContent = --timeRange
+                    if (timeRange <= 0){
+                        clearInterval(tmp)
+                        isFirstElement = false
+                        loop(index+1)
+                    }
+                },1000)
+                return tmp
+            }
+            if (!isFirstElement) var auto = countDown()
+            var previous;
+            controlsBtn.onclick = function(){
+                if (controlsBtn.classList.contains(arg.runBtn)){
+                    controlsBtn.classList.remove(arg.runBtn)
+                    controlsBtn.classList.add(arg.stopBtn)
+                    previous = countDown()
+                }
+                else if (controlsBtn.classList.contains(arg.stopBtn)){
+                    controlsBtn.classList.remove(arg.stopBtn)
+                    controlsBtn.classList.add(arg.runBtn)
+                    clearInterval(previous)
+                    clearInterval(auto)
+                }
+            }
         }
         return
     }
